3b937a92dec1226f3efa84e46032a9ebc58bfa8c
Shekhar Chaudhary <shekhar.0723@gmail.com>
2019-12-17 17:57:18 +0530

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Cutting Stack
+//
+//  Created by Shekhar Chaudhary on 16/09/19.
+//  Copyright © 2019 Shekhar Chaudhary. All rights reserved.
+//
+
+import Foundation
+
+func solve(n: Int, m: Int)->Int{
+ 
+    return (n*m)-1
+}
+
+var n1 = Int(readLine()!)
+var m1 = Int(readLine()!)
+
+print(solve(n: n1!, m: m1!))
+
